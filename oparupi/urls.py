773c084edc544dde64bd1e7aa81b67ed033c6700
Matias Saguir <mativs@gmail.com>
2013-06-23 13:55:52 -0300

    Try zinnia

---
+++ oparupi/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^weblog/', include('zinnia.urls')),
+	url(r'^comments/', include('django.contrib.comments.urls')),
+    # Examples:
+    # url(r'^$', 'oparupi.views.home', name='home'),
+    # url(r'^oparupi/', include('oparupi.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+   )
