147c2ca00f98446ed2d03847c4ae131b53c82f31
Matias Saguir <mativs@gmail.com>
2013-07-29 13:15:37 -0300

    Add post basic model

--- oparupi/conf/django.py
+++ oparupi/conf/django.py
@@ -40,5 +40,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'oparupi.posts',
 )
 
