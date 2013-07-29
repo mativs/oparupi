e925b17e85e1d17bf5c96991ddddcaed9f6407ee
Matias Saguir <mativs@gmail.com>
2013-07-29 14:01:19 -0300

    Add admin to site

--- oparupi/posts/models.py
+++ oparupi/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __unicode__(self):
+        return title
+
+    title = models.CharField(max_length=256)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
