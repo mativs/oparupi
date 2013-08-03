e3e99428c44e72c5f91039a2de9892d94199ae2b
Matias Saguir <mativs@gmail.com>
2013-08-03 17:38:04 -0300

    Start working on top bar

--- oparupi/views.py
+++ oparupi/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from oparupi.posts.models import Post
+from oparupi.posts.models import Post, Section
 
 def home(request, template='home.html', context={}):
-  post = Post.objects.latest('created')
-  context['post'] = post
+  context['sections'] = Section.objects.all()
+  context['post'] = Post.objects.latest('created')
   return render(request, template, context)
 
