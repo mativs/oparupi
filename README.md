a25f2fdc4e1a10a4b6e467e897856b4030496d7d
Matias Saguir <mativs@gmail.com>
2013-08-06 23:02:16 -0300

    working fisotope

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ oparupi
 =======
 
 Oparupi
+
+Requirements
+===========
+
+sudo apt-get install libjpeg62 libjpeg62-dev zlib1g-dev
+
+python manage.py createcachetable oparupi_cache
--- oparupi/static/js/oparupi.js
+++ oparupi/static/js/oparupi.js
@@ -1,7 +1,7 @@
 (function( $ ){
 	$('.posts').fisotope({
 	    itemSelector : '.post',
-	    layoutMode : 'fitRows',
+	    layoutMode : 'masonry',
 	    getSortData : {
 	        title : function ( $elem ) {
 	          return $elem.find('h4').text();
