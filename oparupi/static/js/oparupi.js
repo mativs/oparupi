0490aef34851b76c44f7d356259cb0d6f1d87504
Matias Saguir <mativs@gmail.com>
2013-08-09 21:50:58 -0300

    fisotope initialize empty

--- oparupi/static/js/oparupi.js
+++ oparupi/static/js/oparupi.js
@@ -1,9 +1,11 @@
 (function( $ ){
 	$('.posts').fisotope({
 	    itemSelector : '.post',
+	    filter: '.itecito-1',
 	    layoutMode : 'masonry',
-      default_facet_operator: {
-        sections:'or'
-      }
+	    default_facet_operator: {
+	    	sections:'or'
+	    },
+	    empty_selection_behaviour: "hide"
 	});
 })( jQuery );
