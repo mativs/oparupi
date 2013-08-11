dc67557478eecf8b812a5177d39e092b9ed8b114
Matias Saguir <mativs@gmail.com>
2013-08-10 22:56:13 -0300

    scroll to nav

--- oparupi/static/js/oparupi.js
+++ oparupi/static/js/oparupi.js
@@ -1,13 +1,18 @@
 (function( $ ){
 	$('.posts').fisotope({
-	    itemSelector : '.post',
-	    filter: '.itecito-1',
-	    layoutMode : 'masonry',
-	    default_facet_operator: {
-	    	sections:'or'
+		    itemSelector : '.post',
+	   	 	filter: '.itecito-1',
+	   	 	layoutMode : 'masonry',
+	    	default_facet_operator: {
+	    		sections:'or'
+	    	},
+	    	empty_selection_behaviour: "hide"
 	    },
-	    empty_selection_behaviour: "hide"
-	});
+	    function ( $items ) {
+	    	$.scrollTo($('#nav'));
+	    }
+	);
 
-  $('.post').removeClass('start-hidden')
+  	$('.post').removeClass('start-hidden')
+
 })( jQuery );
