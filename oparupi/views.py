56487c53ef77b10903e31a712454289586dbedbf
Matias Saguir <mativs@gmail.com>
2013-08-16 17:18:05 -0300

    ugly commit with all work at half

--- oparupi/views.py
+++ oparupi/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from oparupi.posts.models import Post, Section
+from taggit.models import Tag
 
 def home(request, template='home.html', context={}):
   context['sections'] = Section.objects.all()
   context['post'] = Post.objects.latest('created')
   context['posts'] = Post.objects.all().order_by('-updated')
+  context['tags'] = Tag.objects.all();
   return render(request, template, context)
 
