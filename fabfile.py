52fe8367dc689b9fad4f7b1b20fe21c5392fca8a
Matias Saguir <mativs@gmail.com>
2013-08-31 19:22:22 -0300

    Commit fabfile

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+from contextlib import contextmanager
+from fabric.api import env, run
+from fabric.context_managers import prefix
+
+env.venv_script = "source venv/bin/activate"
+
+"""
+Local method that runs with /bin/bash
+"""
+def local(command):
+	from fabric import api
+	return api.local(command, shell="/bin/bash") 
+
+def compass():
+	with prefix(env.venv_script):
+		local("python manage.py compass watch &")
+
+def runserver():
+	with prefix(env.venv_script):
+		local("python manage.py runserver")
+
+def setup():
+	local("virtualenv --no-site-packages --distribute venv")		
+	with prefix(env.venv_script):
+		local("pip install -r requirements.txt")
+		local("pyhon manage.py syncdb")
+		local("pyhon manage.py migrate")
+		local("pyhon manage.py loaddata db/posts.json")
+		local("pyhon manage.py loaddata db/tags.json")
+
+def deploy():
+	pass
