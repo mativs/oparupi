df65c5315b06c27b376508a043caa39a87504f86
Matias Saguir <mativs@gmail.com>
2013-09-15 16:30:12 -0300

    Fix static configuration

--- oparupi/conf/static.py
+++ oparupi/conf/static.py
@@ -14,11 +14,11 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = os.path.join(SITE_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
