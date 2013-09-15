ea3fa60fd025e383cd1e6d471d45e937a2912b6d
Matias Saguir <mativs@gmail.com>
2013-09-15 13:15:25 -0300

    Add supervisor

--- fabfile.py
+++ fabfile.py
@@ -11,6 +11,7 @@ env.venv_script = "source venv/bin/activate"
 env.git_uri = "https://github.com/mativs/oparupi.git"
 env.project_name = "oparupi"
 env.domain = "oparupi.com"
+env.project_path = '/home/mativs/oparupi'
 
 def local(command, capture=False):
     """ Implementation that handles local mode or run """
@@ -41,17 +42,8 @@ def setup():
         local("python manage.py loaddata db/posts.json")
         local("python manage.py loaddata db/tags.json")
 
-
-def deploy(db_password):
-    # repository_ensure_apt('ppa:gunicorn/ppa')
-    # package_update()
-    # package_ensure('gunicorn')
-
-    # Clean and Update 
-    package_clean('git')
-    package_update()
-
-    # Database Setup 
+def setup_database(db_password):
+    """ Setup Database on host """
     env.db_password = db_password
     env.db_username = 'oparupi_user'
     env.db_name = 'oparupi_db'
@@ -60,40 +52,64 @@ def deploy(db_password):
     postgresql_role_ensure(env.db_username, env.db_password, createdb=True)
     postgresql_database_ensure(env.db_name, owner=env.db_username,
         locale='en_US.utf8', template='template0', encoding='UTF8')
-    
-    # Source Deploy
+
+def source_deploy():
+    """ Deploy project source on host """
     package_ensure('git')
     if not dir_exists(env.project_path):
         run("git clone %s %s" % (env.git_uri, env.project_path))
 
+def source_update():
+    run("git pull origin master")
     # Source Setup 
     package_ensure('python-dev libpq-dev python-pip python-virtualenv')
 
+def virtualenv_setup():
     with cd(env.project_path):
-        run("virtualenv --no-site-packages --distribute venv")      
-        with prefix(env.venv_script):
+        run("virtualenv --no-site-packages --distribute venv") 
 
-            # Django Setup
-            run("git pull origin master")
-            run("pip install -r requirements.txt")
-            run("cp oparupi/conf/templates/local.prod.py oparupi/conf/local.py")
-            env.djangokey = str(uuid4())
-            file_update('oparupi/conf/local.py', lambda x: text_template(x,env))
+def django_setup():
+    with cd(env.project_path), prefix(env.venv_script):
+        # Django Setup
+        run("git pull origin master")
+        run("pip install -r requirements.txt")
+        run("cp oparupi/conf/templates/local.prod.py oparupi/conf/local.py")
+        env.djangokey = str(uuid4())
+        file_update('oparupi/conf/local.py', lambda x: text_template(x,env))
 
-            # Database Setup
-            python_package_ensure('psycopg2')
-            run("python manage.py syncdb")
-            run("python manage.py migrate")
-            run("python manage.py loaddata db/posts.json")
-            run("python manage.py loaddata db/tags.json")
+def database_setup():
+    with cd(env.project_path), prefix(env.venv_script):
+        python_package_ensure('psycopg2')
+        run("python manage.py syncdb")
+        run("python manage.py migrate")
+        # run("python manage.py loaddata db/posts.json")
+        # run("python manage.py loaddata db/tags.json")
 
-            # Web Server Setup
-            python_package_ensure('gunicorn setproctitle')
-            package_ensure('supervisor')
-            run("cp oparupi/conf/templates/gunicorn.conf.py oparupi/conf/gunicorn.py")
-            file_update('oparupi/conf/gunicorn.py', lambda x: text_template(x,env))
+def gunicorn_setup():
+    with cd(env.project_path), prefix(env.venv_script):
+        python_package_ensure('gunicorn setproctitle')
+        package_ensure('supervisor')
+        run("cp oparupi/conf/templates/gunicorn.conf.py oparupi/conf/gunicorn.py")
+        file_update('oparupi/conf/gunicorn.py', lambda x: text_template(x,env))
+        run("cp oparupi/conf/templates/supervisor.conf /etc/supervisor/conf.d/oparupi.conf")
+        file_update('/etc/supervisor/conf.d/oparupi.conf', lambda x: text_template(x,env))
 
+def host_clean():
+    package_clean('git')
+    package_update()
 
+def deploy(db_password):
+    host_clean()
+    host_update()
+    setup_database(db_password)
+    source_deploy()
+    virtualenv_setup()
+    django_setup()
+    gunicorn_setup()    
+    
+    
+
+           
 #### Posible places to deploy ###
 
 def vagrant():
@@ -102,7 +118,7 @@ def vagrant():
     # connect to the port-forwarded ssh
     env.hosts = ['127.0.0.1:2222']
     # vagrant project path
-    env.project_path = 'oparupi'
+    env.project_path = '/home/vagrant/oparupi'
     # use vagrant ssh key
     result = local('vagrant ssh-config | grep IdentityFile', capture=True)
     env.key_filename = result.split()[1]
